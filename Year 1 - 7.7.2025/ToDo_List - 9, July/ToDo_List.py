d41c5e37cfaa564c3fb941ba6b60725c32c00ac6
PasmaStormer <pasmastormer@gmail.com>
2025-07-12 19:22:04 +0530

    Update ToDo_List.py

--- Year 1 - 7.7.2025/ToDo_List - 9, July/ToDo_List.py
+++ Year 1 - 7.7.2025/ToDo_List - 9, July/ToDo_List.py
@@ -1,1 +1,28 @@
+tasks = ["clean", "wash"]
 
+while True:
+    print("\nWhat do you want to do?")
+    print("1. Add task")
+    print("2. View tasks")
+    print("3. Remove task")
+    print("4. Quit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        adding = input("What task do you want to add?:")
+        tasks.append(adding)
+
+    elif choice == "2":
+         for i in tasks:
+             print(i)
+
+    elif choice == "3":
+        removing = input("What tasks do you want to remove?:")
+        tasks.remove(removing)
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please enter 1, 2, 3, or 4.")
